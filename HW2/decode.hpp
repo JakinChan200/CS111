5d73b8bfdb173e99bfa3526cf68406844f6a0fd8
Jakin Chan <JakinChan200@gmail.com>
2021-04-27 20:45:35 -0700

    Finished the project

--- HW2/decode.hpp
+++ HW2/decode.hpp
@@ -6,18 +6,23 @@
 
 using namespace std;
 
-int findDecodedNum(int secretNum){
-    /*
-    Just need to do this part
-        d = 89
-        n = 187
-        need to do secretNum^89 (rem 187) and return it
-    */
-    return secretNum;
+int findDecodedNum(int coefficient, int secretNum, int exponent, int n){
+    if(exponent == 1){
+        return (secretNum * coefficient) % n;
+    }else if(exponent % 2 == 0){
+        exponent /= 2;
+        secretNum = (secretNum * secretNum) % n;
+        return findDecodedNum(coefficient, secretNum, exponent, n);
+    }else if(exponent % 2 == 1){
+        exponent -= 1;
+        coefficient = (coefficient * secretNum) % n;
+        return findDecodedNum(coefficient, secretNum, exponent, n);
+    }
+    return 0;
 }
 
 char convertToLetter(int decodedNum){
-    decodedNum -= 1;
+    decodedNum -= 3;
     decodedNum += 'a';
     if(decodedNum <= 122)
         return decodedNum;
@@ -30,6 +35,8 @@ char convertToLetter(int decodedNum){
 }  
 
 string DecodeMessage(string fileName){
+    int d = 89;
+    int n = 187;
     vector<int> key(184);
     fill(key.begin(), key.end(), 0);
     ifstream input;
@@ -43,7 +50,7 @@ string DecodeMessage(string fileName){
         input >> current;
         input.ignore();
         if(key[current] == 0){
-            decodedNum = findDecodedNum(current);
+            decodedNum = findDecodedNum(1, current, d, n);
             key[current] = decodedNum;
         }else{
             decodedNum = key[current];
